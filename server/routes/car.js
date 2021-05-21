735f9ac696405cba955d2af777ed4f006b46fa21
tiavina-mika <tiavinamika@gmail.com>
2021-05-21 09:55:00 +0300

    add validation

--- server/routes/car.js
+++ server/routes/car.js
@@ -1,23 +1,28 @@
 import express from 'express';
+import { validate } from 'express-validation';
+
 import carController from '../controllers/car';
+import carValidation from '../validations/car';
+
 // import { authJwt } from '../config/passport'
 
 const router = express.Router()
 
 router.param('carId', carController.carById);
 
-router.route('/api/cars')
+router.route('/cars')
   .get(carController.findAll)
-  .post(carController.create);
+  .post(validate(carValidation.create), carController.create);
+  // .post(validate(carValidation.create), carController.create);
   // .post(authJwt, upload.single('photo'), carController.create);
 
-// router.route('/api/cars/photo/:carId')
+// router.route('/cars/photo/:carId')
 //   .get(carController.photo)
-// router.route('/api/cars/filter/:slug')
+// router.route('/cars/filter/:slug')
 //   .get(carController.findOneBySlug)
-router.route('/api/cars/:carId')
+router.route('/cars/:carId')
   .get(carController.findOne)
-  .put(carController.edit)
+  .put(validate(carValidation.edit), carController.edit)
 //   .put(authJwt, upload.single('photo'), carController.edit)
 //   .delete(authJwt, carController.remove)
   .delete(carController.remove)
